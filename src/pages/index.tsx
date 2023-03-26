a529d2a0e8ec8a07a448a31730b2ed0e1c54f1ab
an.le6 <anle.drewindeed@gmail.com>
2023-03-26 15:49:12 +0700

    7. feat: NoteEditor component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { useState } from "react";
 
 import { Header } from "~/components/Header";
+import { NoteEditor } from "~/components/NoteEditor";
 import { api, type RouterOutputs } from "../utils/api";
 
 const Home: NextPage = () => {
@@ -43,6 +44,27 @@ const Content: React.FC = () => {
     },
   });
 
+  const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
+    {
+      topicId: selectedTopic?.id ?? "",
+    },
+    {
+      enabled: sessionData?.user !== undefined && selectedTopic !== null,
+    }
+  );
+
+  const createNote = api.note.create.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
   return (
     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
       <div className="px-2">
@@ -73,6 +95,28 @@ const Content: React.FC = () => {
               });
               e.currentTarget.value = "";
             }
+          }}
+        />
+      </div>
+      <div className="col-span-3">
+        {/* <div>
+          {notes?.map((note) => (
+            <div key={note.id} className="mt-5">
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </div>
+          ))}
+        </div> */}
+
+        <NoteEditor
+          onSave={({ title, content }) => {
+            void createNote.mutate({
+              title,
+              content,
+              topicId: selectedTopic?.id ?? "",
+            });
           }}
         />
       </div>
