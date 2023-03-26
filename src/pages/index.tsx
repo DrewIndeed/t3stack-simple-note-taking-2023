42ac028eb0e193606895cf9da0aea52f871b19d7
an.le6 <anle.drewindeed@gmail.com>
2023-03-26 15:29:06 +0700

    5. feat: create and view topics column

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useState } from "react";
+
 import { Header } from "~/components/Header";
+import { api, RouterOutputs } from "../utils/api";
 
 const Home: NextPage = () => {
   return (
@@ -12,8 +16,67 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <Header />
+        <Content />
       </main>
     </>
+  );
+};
+
+type Topic = RouterOutputs["topic"]["getAll"][0];
+
+const Content: React.FC = () => {
+  const { data: sessionData } = useSession();
+  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
+
+  // get all topics
+  const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
+    undefined, // no input
+    {
+      enabled: sessionData?.user !== undefined,
+    }
+  );
+
+  // create new topic topics
+  const createTopic = api.topic.create.useMutation({
+    onSuccess: () => {
+      void refetchTopics();
+    },
+  });
+
+  return (
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a
+                href="#"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  setSelectedTopic(topic);
+                }}
+              >
+                {topic.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="divider"></div>
+        <input
+          type="text"
+          placeholder="New Topic"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createTopic.mutate({
+                title: e.currentTarget.value,
+              });
+              e.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+    </div>
   );
 };
 
